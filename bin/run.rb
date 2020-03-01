f111e8d02104a9d12032fe4d15fbcb96c7aaccbf
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-01 19:58:31 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,4 @@
-#require_relative 'environment'
-
+require_relative 'environment'
 db = SQLite3::Database.new('../db/lab_database.db')
 sql_runner = SQLRunner.new(db)
-
 sql_runner.execute_sql_file
-
-
