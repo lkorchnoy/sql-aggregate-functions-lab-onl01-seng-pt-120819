dae6973a1fb4ebc71eda674ca7be004d3d9e6ed4
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-01 19:45:19 +0000

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -3,3 +3,8 @@ require 'bundler/setup'
 Bundler.require
 
 require_relative 'sql_runner'
+
+require 'sqlite3'
+require_relative '../lib/students.rb'
+ 
+db = SQLite3::Database.new('../db/students_database.db')
--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,30 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+  "SELECT MAX(gpa) FROM students;"
+  #db.execute(highest_student_gpa())
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
+  #db.execute(lowest_student_gpa())
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
+  #db.execute(average_student_gpa())
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students;"
+  #db.execute(total_tardies_for_all_students())
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade == 9;"
+  #db.execute(average_gpa_for_9th_grade())
 end
+#file.each_line do |line|
+ # string << average_gpa_for_9th_grade(line)
+
+
+
