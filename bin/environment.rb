dc680cac5898883de5fc69fc8c8647153bf05072
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-01 20:00:57 +0000

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -1,10 +1,3 @@
 require 'bundler/setup'
-
 Bundler.require
-
 require_relative 'sql_runner'
-
-require 'sqlite3'
-require_relative '../lib/students.rb'
- 
-db = SQLite3::Database.new('../db/students_database.db')
--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -1,13 +1,10 @@
 require_relative 'environment'
 
 class SQLRunner
-
   COMMANDS = ["create", "insert"]
-
   def initialize(db)
     @db = db
   end
-
   def self.make_methods
     COMMANDS.each do |command|
       define_method("execute_sql_#{command}") do
@@ -16,8 +13,26 @@ class SQLRunner
       end
     end
   end
-
   def execute_sql(sql)
      sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
